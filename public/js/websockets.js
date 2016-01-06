adc3b6db7b536d4814181ccb9b7b761a3d8aab62
Maxim Ryndin <rgmasim@gmail.com>
2016-01-06 14:01:42 +0300

    Add some debug info

--- public/js/websockets.js
+++ public/js/websockets.js
@@ -2,3 +2,6 @@ var socket = io.connect('http://localhost:3000/');
 socket.on('error', function (error) {
   console.log('Error on socket', error);
 });
+socket.on('refreshActs', function(data){
+	console.log('refreshActs', data);
+})
