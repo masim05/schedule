9306fe5ff372d30646374d897c34c4905070150f
Maxim Ryndin <rgmasim@gmail.com>
2016-01-06 13:34:18 +0300

    Add controllers.

---
+++ controllers/createAct.js
@@ -0,0 +1,31 @@
+fs = require('fs');
+
+module.exports = function createAct(filename, socket) {
+  var controller = function(data) {
+    console.log('createAct called.');
+    fs.readFile(filename, function(err, content) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      try {
+        acts = JSON.parse(content);
+      } catch (err) {
+        console.error(err.stack);
+        return;
+      }
+
+      acts.unshift(data.act);
+
+      fs.writeFile(filename, JSON.stringify(acts), function (err) {
+        if (err) {
+          console.error(err.stack);
+          return;
+        }
+      });
+    });
+  };
+
+  return controller;
+};
---
+++ controllers/refreshActs.js
@@ -0,0 +1,25 @@
+fs = require('fs');
+
+module.exports = function refreshActs(filename, emitable) {
+    console.log('refreshActs called.');
+
+    if(!emitable) {
+      return;
+    }
+
+    fs.readFile(filename, function(err, content) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      try {
+        acts = JSON.parse(content);
+      } catch (err) {
+        console.error(err.stack);
+        return;
+      }
+
+      return emitable.emit('refreshActs', {acts: acts});
+    });
+};
