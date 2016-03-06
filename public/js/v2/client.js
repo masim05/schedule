a8bbc82b99ef315fb328204285497a1cfb62b3f1
Maxim Ryndin <masim_r@mail.ru>
2016-03-06 10:37:49 +0300

    Unbreak creation of the acts.

--- public/js/v2/client.js
+++ public/js/v2/client.js
@@ -1,3 +1,5 @@
+var socket;
+
 var ScheduleBox = React.createClass({
   getInitialState: function () {
     return {
@@ -15,15 +17,15 @@ var ScheduleBox = React.createClass({
       return out;
     }
 
-    socket = this.socket = io(normalizeUrl(document.location.href));
-    this.socket.on('disconnect', function () {
+    socket = io(normalizeUrl(document.location.href));
+    socket.on('disconnect', function () {
       console.log('disconnect fired.');
     });
-    this.socket.on('connect', function () {
+    socket.on('connect', function () {
       console.log('connect fired.');
 
     });
-    this.socket.on('error', function (error) {
+    socket.on('error', function (error) {
       console.error(error);
     });
 
@@ -35,7 +37,7 @@ var ScheduleBox = React.createClass({
     */
   },
   componentWillUnmount: function () {
-    this.socket.close();
+    socket.close();
   },
   render: function () {
     var scheduleBox = this;
@@ -53,7 +55,7 @@ var ScheduleBox = React.createClass({
     } else {
       node = (
         <div>
-          0000000
+          <ActForm/>
         </div>
       );
     }
@@ -69,7 +71,6 @@ var ScheduleBox = React.createClass({
             </div>
           </div>
         </div>
-
         {node}
       </div>
     )
@@ -120,6 +121,135 @@ var MainMenuItems = React.createClass({
       <ul className="nav navbar-nav navbar-left">
         {items}
       </ul>
+    )
+  }
+});
+
+var ActForm = React.createClass({
+  getInitialState: function () {
+    var now = moment();
+
+    return {
+      'act.start.date': now.format('YYYY-MM-DD'),
+      'act.start.time': now.format('HH:mm'),
+      'act.type': 'Сон',
+      'act.comment': ''
+    };
+  },
+  componentDidMount: function () {
+    var self = this;
+    var datetimeInterval = setInterval(function () {
+      var now = moment();
+      self.setState({
+        'act.start.date': now.format('YYYY-MM-DD'),
+        'act.start.time': now.format('HH:mm')
+      });
+    }, 2 * 60 * 1000);
+
+    self.setState({
+      datetimeInterval: datetimeInterval
+    });
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.state.datetimeInterval);
+  },
+  handleDateChange: function (e) {
+    this.setState({
+      'act.start.date': e.target.value
+    });
+  },
+  handleTimeChange: function (e) {
+    this.setState({
+      'act.start.time': e.target.value
+    });
+  },
+  handleTypeChange: function (e) {
+    this.setState({
+      'act.type': e.target.value
+    });
+  },
+  handleCommentChange: function (e) {
+    this.setState({
+      'act.comment': e.target.value
+    });
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+    if (!this.state['act.start.date'] || !this.state['act.start.time'] || !this.state['act.type']) {
+      return;
+    }
+    socket.emit('createAct', {
+      act: {
+        start: {
+          date: this.state['act.start.date'],
+          time: this.state['act.start.time']
+        },
+        type: this.state['act.type'],
+        comment: this.state['act.comment']
+      }
+    });
+
+    var now = moment();
+    this.setState({
+      'act.start.date': now.format('YYYY-MM-DD'),
+      'act.start.time': now.format('HH:mm'),
+      'act.type': 'Сон',
+      'act.comment': ''
+    });
+  },
+  render: function () {
+    var self = this;
+    var typeNodes = ['Сон', 'Кормление', 'Активное бодрствование'].map(function (type, id) {
+      return (
+        <label
+          htmlFor={'type-' + id}
+          className={"btn btn-primary" + (self.state["act.type"] == type ? ' active' : '')}
+          key={id}>
+          {type}
+          <input type="radio" name="type" className="hidden" value={type} id={'type-' + id}
+                 checked={self.state["act.type"] == type} onChange={self.handleTypeChange}/>
+        </label>
+      )
+    });
+    return (
+      <form className="actForm" onSubmit={this.handleSubmit}>
+        <h3>Новая активность</h3>
+        Дата:
+        <input
+          type="date"
+          className="form-control"
+          value={this.state["act.start.date"]}
+          onChange={this.handleDateChange}
+        />
+        <br/>
+        Время:
+        <input
+          type="time"
+          className="form-control"
+          value={this.state["act.start.time"]}
+          onChange={this.handleTimeChange}
+        />
+        <br/>
+
+        Занятие:<br/>
+        <div className="btn-group" data-toggle="buttons">
+          {typeNodes}
+        </div>
+        <br/><br/>
+
+        Комментарий:
+        <input
+          type="text"
+          className="form-control"
+          value={this.state["act.comment"]}
+          onChange={this.handleCommentChange}
+        />
+        <br/>
+        <input
+          type="submit"
+          className="btn btn-lg btn-primary btn-block"
+          value="Начать"/>
+      </form>
     )
   }
 });
