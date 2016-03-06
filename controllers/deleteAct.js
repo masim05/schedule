03afc15c84568e8986e7c85c39f9d3def17001e1
Maxim Ryndin <masim_r@mail.ru>
2016-03-06 11:01:40 +0300

    Correct code style.

--- controllers/deleteAct.js
+++ controllers/deleteAct.js
@@ -2,9 +2,9 @@ fs = require('fs');
 _ = require('lodash');
 crypto = require('crypto');
 
-module.exports = function deleteAct(filename, socket) {
-  var controller = function(data) {
-    fs.readFile(filename, function(err, content) {
+module.exports = function deleteAct(filename) {
+  var controller = function (data) {
+    fs.readFile(filename, function (err, content) {
       if (err) {
         console.error(err);
         process.exit(1);
@@ -12,12 +12,13 @@ module.exports = function deleteAct(filename, socket) {
 
       try {
         var acts = JSON.parse(content);
-      } catch (err) {
+      }
+      catch (err) {
         console.error(err.stack);
         return;
       }
 
-      _.remove(acts, function(a) {
+      _.remove(acts, function (a) {
         return a.id == data.act.id;
       })
 
