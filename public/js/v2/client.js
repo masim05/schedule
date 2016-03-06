24023332d08e8c8c71e46e41e87958730717f6f4
Maxim Ryndin <masim_r@mail.ru>
2016-03-06 11:14:05 +0300

    Implement acts logick at the v2 page.

--- public/js/v2/client.js
+++ public/js/v2/client.js
@@ -46,6 +46,7 @@ var ScheduleBox = React.createClass({
       node = (
         <div>
           <ActForm/>
+          <ActList/>
         </div>
       );
     }
@@ -240,6 +241,165 @@ var ActForm = React.createClass({
           className="btn btn-lg btn-primary btn-block"
           value="Начать"/>
       </form>
+    )
+  }
+});
+
+var ActList = React.createClass({
+  getInitialState: function () {
+    return {
+      acts: []
+    }
+  },
+  componentDidMount: function () {
+    socket.on('refreshActs', function (data) {
+      this.setState(data);
+    }.bind(this));
+    socket.emit('reqRefreshActs');
+  },
+  componentWillUnmount: function () {
+    socket.removeListener('refreshActs');
+  },
+  render: function () {
+    var actNodes = this.state.acts
+      .map(function (act) {
+        return (
+          <Act act={act} key={act.id}/>
+        )
+      });
+    return (
+      <div className="actList">
+        <h3>Недавние активности</h3>
+        {actNodes}
+      </div>
+    );
+  }
+});
+var Act = React.createClass({
+  getInitialState: function () {
+    var now = moment();
+    return {
+      'act.finish.date': now.format('YYYY-MM-DD'),
+      'act.finish.time': now.format('HH:mm')
+    };
+  },
+  componentDidMount: function () {
+    var self = this;
+    var datetimeInterval = setInterval(function () {
+      var now = moment();
+      self.setState({
+        'act.finish.date': now.format('YYYY-MM-DD'),
+        'act.finish.time': now.format('HH:mm')
+      });
+    }, 2 * 60 * 1000);
+
+    self.setState({
+      datetimeInterval: datetimeInterval
+    });
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.state.datetimeInterval);
+  },
+  handleTimeChange: function (e) {
+    this.setState({
+      'act.finish.time': e.target.value
+    });
+  },
+  handleFinish: function (e) {
+    e.preventDefault();
+    var id = this.props.act.id;
+    var self = this;
+    socket.emit('finishAct', {
+      act: {
+        id: id,
+        finish: {
+          date: self.state["act.finish.date"],
+          time: self.state["act.finish.time"]
+        }
+      }
+    });
+  },
+  handleDelete: function (e) {
+    e.preventDefault();
+    var id = this.props.act.id;
+    socket.emit('deleteAct', {
+      act: {
+        id: id
+      }
+    });
+  },
+  render: function () {
+    // TODO Remove ternary operator when data is consistent
+    var topProperties = [
+      <div className="col-md-2 col-xs-4" key="date">{this.props.act.start.date}</div>
+    ];
+    if (this.props.act.finish) {
+      topProperties.push(
+        <div className="col-md-6 col-xs-6" key="time">
+          {this.props.act.start.time} - {this.props.act.finish.time}
+        </div>
+      )
+    } else {
+      topProperties.push(
+        <div className="col-md-6 col-xs-6" key="time">
+          {this.props.act.start.time} -
+          <input
+            type="time"
+            className="form-control"
+            value={this.state["act.finish.time"]}
+            onChange={this.handleTimeChange}
+          />
+        </div>
+      )
+    }
+    if (!this.props.act.finish) {
+      topProperties.push(
+        <div className="col-md-1 col-xs-2" key="button">
+          <input
+            type="submit"
+            className="btn btn-primary btn-block btn-xs"
+            value="F"/>
+        </div>
+      );
+    }
+    var topLine = <div className="row">
+      <div id="equalheight">
+        {topProperties}
+      </div>
+    </div>;
+
+    var bottomProperties = [
+      <div className="col-md-2 col-xs-4" key="type">{this.props.act.type}</div>
+    ];
+    if (this.props.act.comment) {
+      bottomProperties.push(
+        <div className="col-md-6 col-xs-6 light" key="comment">{this.props.act.comment}</div>
+      );
+    }
+    ;
+    bottomProperties.push(
+      <div className="col-md-1 col-xs-2" key="button">
+        <input
+          type="submit"
+          className="btn btn-primary btn-block btn-xs"
+          value="D"/>
+      </div>
+    );
+
+    var bottomLine = <div className="row">
+      {bottomProperties}
+    </div>;
+
+    return (
+      <div className="act">
+        <form className="actForm" onSubmit={this.handleFinish}>
+          {topLine}
+        </form>
+        <form className="actForm" onSubmit={this.handleDelete}>
+          {bottomLine}
+        </form>
+        <hr/>
+      </div>
     )
   }
 });
